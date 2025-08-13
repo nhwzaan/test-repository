5bcd785cdb0e69f731c7817621e05d2d9b0995d5
Van Nguyen Thi Nhu <ngthnhuvan@gmail.com>
2025-08-13 10:34:49 +0700

    test commit 8

---
+++ documents.py
@@ -0,0 +1,48 @@
+documents = [
+    {
+        "id": "15c962433e632147b96d4a48ccc09990fec30ade",
+        "metadata": {
+            "file_path": "README.md",
+            "file_name": "README.md",
+            "url": "https://api.github.com/nhwzaan\\test-repository\\blob/main\\README.md"
+        },
+        "text": "# test-repository"
+    },
+    {
+        "id": "b2cdc934ff6a4a1d677dddfa1758d523633d23e0",
+        "metadata": {
+            "file_path": "test.py",
+            "file_name": "test.py",
+            "url": "https://api.github.com/nhwzaan\\test-repository\\blob/main\\test.py"
+        },
+        "text": (
+            'print("Hello, World!")\n'
+            'a = 5\n'
+            'b = 10\n'
+            'c = a + b\n'
+            'print("The sum of", a, "and", b, "is", c)\n'
+            '# This is a simple Python script to demonstrate basic arithmetic operations.'
+        )
+    },
+    {
+        "id": "8d2c61b1d635fee72c4ce11276235008e44169a7",
+        "metadata": {
+            "file_path": "test2.py",
+            "file_name": "test2.py",
+            "url": "https://api.github.com/nhwzaan\\test-repository\\blob/main\\test2.py"
+        },
+        "text": (
+            'print("Wish it will work!")\n'
+            'print("Wish it will work!")'
+        )
+    },
+    {
+        "id": "702d670f5e3c2295ea40cf41fe4579b58247a972",
+        "metadata": {
+            "file_path": "test3.py",
+            "file_name": "test3.py",
+            "url": "https://api.github.com/nhwzaan\\test-repository\\blob/main\\test3.py"
+        },
+        "text": 'print(123)'
+    }
+]
